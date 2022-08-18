0feed31aaf7312dba1477085fb4a0008c5ca12dd
Juan Felipe Marin <felipemarin1109@gmail.com>
2022-08-18 14:20:48 -0500

    Nuevos ejercicios

--- taller2.js
+++ taller2.js
@@ -3,3 +3,5 @@ var numero1 = parseFloat(prompt('Ingrese un numero'))
 var numero2 = parseFloat(prompt('Ingrese otro numero'))
 var resultado = numero1 + numero2
 alert('La suma es:', resultado)
+
+// Ejercicio 2
